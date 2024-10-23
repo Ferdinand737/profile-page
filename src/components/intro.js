463b365e5937af6c63b94012ce01501c80350fc3
Ferdinand <ferdinandhaaben@proton.me>
2024-10-22 19:02:07 -0700

    update image

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,10 +1,10 @@
-export default function Intro(props){
-    return (
-      <div className="intro-section">
-        <img src={require('../images/profilePic.jpeg')} width='200' height='200' alt='me' className='intro-image'></img>
-        <div className="intro-paragraph">
-            {props.children}
-        </div>
+export default function Intro(props) {
+  return (
+    <div className="intro-section">
+      <img src={require('../images/profilePic.png')} width='200' height='200' alt='me' className='intro-image'></img>
+      <div className="intro-paragraph">
+        {props.children}
       </div>
-    )
+    </div>
+  )
 } 
