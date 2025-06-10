c72abf4d884e9787a5173affb0fac6052dfd09d4
Ferdinand Haaben <71345367+Ferdinand737@users.noreply.github.com>
2025-06-09 17:50:54 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git pull origin master
 npm install
 npm run build
 pm2 delete ferdinand-profile || true
-pm2 start npm --name ferdinand-profile --env PORT=3001 --cwd build
+pm2 start serve --name ferdinand-profile --env PORT=3001 --cwd build
 pm2 save
 pm2 restart ferdinand-profile
